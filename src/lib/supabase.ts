98c081903e331ae8b75936d4cbf7b1ed88676610
zubbicodes <zubbihawk@proton.me>
2025-10-29 15:26:28 +0500

    Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://tstjwjgsretjynxydjdv.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzdGp3amdzcmV0anlueHlkamR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MjAxMDUsImV4cCI6MjA3NTQ5NjEwNX0.9cC4UYB_S96DLkX94jzG4zvbtXX8oLAiEmSLf_BvUT0";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
